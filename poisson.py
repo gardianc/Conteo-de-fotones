96fdf13cb32070f03689de8a4fd905dc326f6c3f
gardianc <pau.mgarbino@gmail.com>
2024-02-08 20:40:31 -0300

    análisis de la fuente estable

--- poisson.py
+++ poisson.py
@@ -12,10 +12,11 @@ from scipy.optimize import curve_fit
 def poisson(x,L):
     p=np.exp(-L)*L**x/np.math.factorial(x)
     return p
-fotones=np.loadtxt('laserestable.txt')
+fotones=np.loadtxt('Mediciones/laserestable.txt')
 plt.figure()
 maximo=int(max(fotones))
-n,bins,patches=plt.hist(fotones,bins=np.linspace(0,maximo,maximo+1))
+n,bins,patches=plt.hist(fotones,bins=np.linspace(0,maximo,maximo+1), align='left')
 bins0=np.delete(bins,len(bins)-1)
 bins1=bins0+0.5
 plt.plot(bins1,n,'.-')
+plt.show()
