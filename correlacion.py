6485bb54301258c426860b434ede21df2cd0e7c0
gardianc <pau.mgarbino@gmail.com>
2024-02-13 21:58:29 -0300

    mediciones de autpcorrelación fuente lenta, correcciones al código

--- correlacion.py
+++ correlacion.py
@@ -19,13 +19,24 @@ mpl.rcParams['axes.grid.axis'] = 'both'
 mpl.rcParams['axes.axisbelow'] = True
 
 # figure:
+mpl.rcParams['figure.figsize'] = [7.4, 4.8]
 mpl.rcParams['figure.autolayout'] = True
 mpl.rcParams['figure.dpi'] = 100
 
+# fontsizes
+mpl.rcParams['xtick.labelsize'] = 12
+mpl.rcParams['ytick.labelsize'] = 12
+mpl.rcParams['legend.fontsize'] = 12
+mpl.rcParams['legend.title_fontsize'] = 12
+mpl.rcParams['axes.labelsize'] = 16
+
+# save
+mpl.rcParams['savefig.bbox'] = 'tight'
+
 #%%
 
-tiempo = np.loadtxt('Mediciones/medicioncorrelación tiempos0.csv', delimiter=',')*1e3 # ms
-data = np.loadtxt('Mediciones/medicioncorrelación voltajes0.csv', delimiter=',') # V
+tiempo = np.loadtxt('Mediciones/mediciones autocorr 3/medicioncorrelación lento 1.5V tiempos0.csv', delimiter=',')*1e3 # ms
+data = np.loadtxt('Mediciones/mediciones autocorr 3/medicioncorrelación lento 1.5V voltajes0.csv', delimiter=',') # V
 voltajes = data - np.mean(data)
 
 intervalo_temp = tiempo[-1]-tiempo[-2] # espacio entre dos samples
@@ -42,11 +53,13 @@ print(f'el ancho en samples de la primer campana es: {cruce}\nel tiempo de coher
 fig, ax = plt.subplots()
 ax.axhline(0, color='k')
 # ax.plot(np.arange(len(corr_positivo))*intervalo_temp, corr_positivo, color='k')
-ax.plot(np.arange(500)*intervalo_temp, corr_positivo[:500], color='k')
-ax.plot(tiempo_de_coherencia, corr_positivo[cruce], 'o', color='b', label=f't = {tiempo_de_coherencia}')
+ax.plot(np.arange(2000)*intervalo_temp, corr_positivo[:2000], color='k')
+ax.plot(tiempo_de_coherencia, corr_positivo[cruce], 'o', color='r', label=f't = 97.0')
 ax.set_xlabel('Tiempo [ms]')
 ax.set_ylabel('Autocorrelación') # ver si se normaliza o cómo son las unidades
 ax.legend()
+print(tiempo_de_coherencia*50/1000000, intervalo_temp)
+plt.savefig('Gráficos/autocorr 1.9.pdf', format='pdf', dpi=150)
 plt.show()
 
 #%% tc promedio de 100 pantallas del osciloscopio (sin resistencia)
@@ -55,7 +68,7 @@ tc_100 = []
 N = 100
 
 for n in range(N):
-    data = np.loadtxt(f'Mediciones/medicioncorrelación voltajes{n}.csv', delimiter=',') # V
+    data = np.loadtxt(f'Mediciones/mediciones autocorr 3/medicioncorrelación lento 1.5V voltajes{n}.csv', delimiter=',') # V
     voltajes = data - np.mean(data)
     corr = np.correlate(voltajes-np.mean(voltajes),voltajes-np.mean(voltajes),mode='full')
     corr_positivo = corr[2499:] # corto sólo para los positivos
